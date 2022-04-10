0c24d293a217e1e2828a9ce634c42c0b29f1cfb8
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-04-09 21:39:39 -0300

    refactor - remove código não utilizado

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Login from './pages/Login';
 import Game from './pages/Game';
 import Feedback from './pages/Feedback';
 import Ranking from './pages/Ranking';
-import Config from './pages/Config';
 
 export default function App() {
   return (
@@ -16,7 +15,6 @@ export default function App() {
           <Route exact path="/game" component={ Game } />
           <Route exact path="/feedback" component={ Feedback } />
           <Route exact path="/ranking" component={ Ranking } />
-          <Route exact path="/config" component={ Config } />
         </Switch>
       </header>
     </div>
